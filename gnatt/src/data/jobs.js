81ab4d7fe7b9b3560fb59a0d20796ba7f92df835
Ben Leatherman <bjleatherman@gmail.com>
2025-04-16 19:26:11 -0400

    feature: added jobs json obj

---
+++ gnatt/src/data/jobs.js
@@ -0,0 +1,16 @@
+const jobs = [
+    {
+        "JobNum" : "PAA5150",
+        "Phase" : "ICR", 
+        "ExpectedStartDate" : "3/25/2025", 
+        "ActualStartDate" : "3/25/2025", 
+        "UserOverwriteStartDate" : "", 
+        "ExpectedEndDate" : "3/26/2025", 
+        "ClientAgreedEndDate" : "", 
+        "ActualEndDate" : "3/26/2025", 
+        "UserOverwriteEndDate" : "", 
+        "Analysts" : ["Ben"]
+    },
+]
+
+export default jobs;
